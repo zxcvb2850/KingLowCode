2ad48a373d9aced2a401933b7131a3f9e959599a
zxcvb2850 <zxcvb2850@163.com>
2022-08-09 01:35:54 +0800

    style: update code

--- front/src/pages/ContainerCenter/index.tsx
+++ front/src/pages/ContainerCenter/index.tsx
@@ -1,17 +1,20 @@
 import React, { KeyboardEvent, ReactElement } from "react";
 import { useRecoilState } from "recoil";
 import store from "../../store";
-import { CustomReactPortal } from "../../store/module/home";
+import { CustomReactPortal, domData } from "../../store/module/home";
 import "./index.less";
 import lodash from "lodash";
+import { getClassName, addClassName, removeClassName } from "../../utils/ClassName";
 
 const ContainerCenter = () => {
   const [dom, setDom] = useRecoilState(store.home.domData);
 
   // 给每个组件添加点击事件
   const addClickProps = (itemProps: any, children: CustomReactPortal) => {
+    console.log("itemProps", itemProps);
     return {
       ...itemProps,
+      className: addClassName(itemProps.className, ["k-hover"]),
       onClick: (e: KeyboardEvent<HTMLElement>) => handleClick(e, children),
     };
   };
@@ -28,62 +31,45 @@ const ContainerCenter = () => {
     let p1 = lodash.cloneDeep(dom);
     let p2 = p1;
     let cur = null;
-    console.log("className", withClassName(curDom.props.className, "get"));
-    while (p2 && typeof p2 !== "string" && typeof p2 !== "number") {
-      const { className } = p2.props;
-      p2.props.className = withClassName(className, "remove", ["k-select"]);
-      if (p2.key === curDom.key) {
-        cur = p2;
-        p2.props.className = withClassName(className, "add", ["k-select"]);
+    console.log("className", getClassName(curDom.props.className));
+    // while (p2 && typeof p2 !== "string" && typeof p2 !== "number") {
+    //   const { className } = p2.props;
+    //   p2.props.className = removeClassName(className, ["k-select"]);
+    //   if (p2.key === curDom.key) {
+    //     cur = p2;
+    //     p2.props.className = addClassName(className, ["k-select"]);
+    //   }
+    //   // p2 = p2.children as CustomReactPortal;
+    // }
+    // setDom(p1);
+  };
+
+  const renderItemChildren = (dom: CustomReactPortal[]): ReactElement[] | null => {
+    if(!dom) return null
+    console.log("dom", dom)
+    if (typeof dom === "string") return dom; 
+    const doms: ReactElement[] = [];
+    const len = dom.length;
+    for (let i = 0; i< len; i++) {
+      const item = dom[i];
+      if (item?.tag === 1) {
+        doms.push(React.createElement(item.type, addClickProps({key: item.key, ...item.props}, item), item.children.join()));
+      } else {
+        // @ts-ignore
+        doms.push(React.createElement(item.type, addClickProps({key: item.key, ...item.props}, item), renderItemChildren(item.children)));
       }
-      p2 = p2.children as CustomReactPortal;
     }
-    setDom(p1);
-  };
 
-  // 处理className
-  const withClassName = (
-    className: string | null,
-    type: "get" | "add" | "remove",
-    target?: string[]
-  ): string => {
-    let classNames: string[] = [];
-    if (!!className) {
-      className = className.trim();
-      classNames = className?.split(" ") || []; // 查询原始 class
-    }
-    if (type === "add") {
-      // @ts-ignore
-      classNames = lodash.uniq([...target, ...classNames]); // 去重
-    } else if (type === "remove") {
-      // @ts-ignore
-      classNames = classNames.filter((n) => !(target.indexOf(n) > -1));
-    }
-    return classNames.join(" ");
-  };
-
-  // 渲染内容
-  const childrenContent = (
-    children: CustomReactPortal
-  ): ReactElement | null => {
-    let type = children.type;
-    if (!children || children == null) {
-      return null;
-    } else if (typeof children === "string" || typeof children === "number") {
-      return children;
-    } else {
-      return React.createElement(
-        type,
-        addClickProps(children.props, children),
-        childrenContent(children.children as CustomReactPortal)
-      );
-    }
-  };
+    return doms;
+  }
 
   return React.createElement(
     "div",
     { className: "k-container-center" },
-    childrenContent(dom)
+    // childrenContent(dom)
+    renderItemChildren(dom)
+    // React.createElement(dom[0].type, dom[0].props, dom[0].children),
+    // React.createElement(dom[1].type, dom[1].props, dom[1].children)
   );
 };
 
--- front/src/store/module/home.ts
+++ front/src/store/module/home.ts
@@ -1,29 +1,79 @@
-import { ReactPortal } from 'react';
-import {atom} from "recoil";
+import { Fragment, ReactPortal } from 'react';
+import { atom } from "recoil";
 import KButton from '../../components/KButton';
 
 export interface CustomReactPortal extends ReactPortal {
     isCustomComponent?: boolean
-    children: CustomReactPortal | string
+    children: (CustomReactPortal | string)[]
+    tag?: number // 标签类型 1 纯文本
 }
 
-export const domData = atom<CustomReactPortal>({
+export const domData = atom<CustomReactPortal[]>({
     key: "update_dom",
-    default: {
+    default: [{
         key: 1,
         type: "div",
         props: {},
-        children:  {
+        children: [{
+            key: 3,
+            tag: 1,
+            type: "p",
+            props: {},
+            children: ["this is P 1"],    
+        }, {
+        key: 4,
+        type: "div",
+        props: {},
+        children: [
+            {
+                key: 5,
+                tag: 1,
+                type: "span",
+                props: {},
+                children: ["this is P 1-1"],    
+            },
+            {
+                key: 6,
+                tag: 1,
+                type: "p",
+                props: {},
+                children: ["this is P 1-2"],    
+            }   
+        ],    
+    }]
+    }, {
+        key: 2,
+        type: "div",
+        props: {},
+        children: [{
+            key: 4,
+            tag: 1,
+            type: "p",
+            props: {},
+            children: ["this is P 2"],    
+        }]
+    }],
+})
+
+/*
+children: [{
             key: 2,
             type: "p",
             props: {},
-            children: {
+            children: [{
                 key: 3,
                 type: KButton,
                 props: {},
                 isCustomComponent: true,
-                children:"KButton",
-            }
-        }
-    },
-})
+                children: ["KButton"],
+            },
+            {
+                key: 4,
+                type: KButton,
+                props: {},
+                isCustomComponent: true,
+                children: ["KButton"],
+            }]
+        }]
+
+*/
