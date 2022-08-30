bc8231cf5860b7ff96611a16160775b93800552a
zxcvb2850 <zxcvb2850@163.com>
2022-08-30 19:25:25 +0800

    style: container style change

--- front/src/pages/ContainerLeft/index.tsx
+++ front/src/pages/ContainerLeft/index.tsx
@@ -1,133 +1,77 @@
 import React from "react";
-import {Alert, Avatar, Badge, Button, Layout} from "antd";
-import KButton from "../../components/Template/KingUi/KButton";
+import {Divider, Collapse} from "antd";
 import "./index.less";
-import AAlert from "../../components/Template/AntdUi/AButton";
+
+const {Panel} = Collapse;
+
+interface ComArrFace {
+    type: string
+    title: string
+    components: {
+        name: string
+        data: string
+    }[]
+}
+
+const componentsArray: ComArrFace[] = [
+    {
+        type: "basic", title: "基础组件", components: [
+            {name: "按钮", data: "AButton"},
+        ]
+    },
+    {
+        type: "layout", title: "布局组件", components: [
+            {name: "布局容器", data: "ALayout"},
+            {name: "顶部布局", data: "AHeader"},
+        ]
+    },
+    {
+        type: "form", title: "表单组件", components: [
+            {name: "输入框", data: "AInput"},
+            {name: "警告提示", data: "AAlert"},
+        ]
+    }
+];
 
 const ContainerLeft = () => {
-  const handleClick = () => {};
+    const handleClick = () => {
+    };
 
-  // 拖拽开始
-  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
-    const componentName = (e.target as HTMLDivElement).dataset.name ?? "";
-    const componentUi = (e.target as HTMLDivElement).dataset.ui ?? "";
-    e.dataTransfer.setData("componentName", componentName); // 拖拽数据穿透
-    e.dataTransfer.setData("componentUi", componentUi); // 拖拽数据穿透
-  };
+    // 拖拽开始
+    const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+        const componentName = (e.target as HTMLDivElement).dataset.name ?? "";
+        const componentUi = (e.target as HTMLDivElement).dataset.ui ?? "";
+        e.dataTransfer.setData("componentName", componentName); // 拖拽数据穿透
+        e.dataTransfer.setData("componentUi", componentUi); // 拖拽数据穿透
+    };
 
-  // 拖拽结束
-  const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-    console.log("end event", e);
-  };
+    // 拖拽结束
+    const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log("end event", e);
+    };
 
-  return (
-    <div className="k-container-left">
-      <h2>组件</h2>
-        <div
-            className="item-component-example"
-            data-name="KButton"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-            按钮
+    return (
+        <div className="k-container-left">
+            <Collapse accordion defaultActiveKey="basic">
+                {componentsArray.map(item => (
+                    <Panel header={item.title} key={item.type}>
+                        <div className="component-thumb-wrap">
+                            {item.components.map(c => (
+                                <div
+                                    key={c.name}
+                                    className="item-component-example"
+                                    data-name={c.data}
+                                    draggable
+                                    onDragStart={handleDragStart}
+                                    onDragEnd={handleDragEnd}
+                                >{c.name}</div>
+                            ))}
+                        </div>
+                    </Panel>
+                ))}
+            </Collapse>
         </div>
-        <div
-            className="item-component-example"
-            data-name="KAlert"
-            data-ui="king"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-            提示
-        </div>
-      <div className="component-example-wrap">
-        <div
-            className="item-component-example"
-            data-name="AButton"
-            data-ui="antd"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <Button type="primary">按钮</Button>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="AAlert"
-            data-ui="antd"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <Alert message={"提示"}/>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="ABadge"
-            data-ui="antd"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <Badge>
-            <Avatar size="large" shape="square"/>
-          </Badge>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="AHeader"
-            data-ui="antd"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <Layout>Layout Header</Layout>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="ALayout"
-            data-ui="antd"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <Layout>Layout</Layout>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="AAvatar"
-            data-ui="antd"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <Avatar/>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="KAlert"
-            data-ui="kingUi"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <span>KAlert</span>
-        </div>
-        <div
-            className="item-component-example"
-            data-name="KButton"
-            data-ui="kingUi"
-            draggable
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-        >
-          <span>KButton</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ContainerLeft;
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -3,17 +3,33 @@ import ContainerLeft from './ContainerLeft';
 import ContainerCenter from './ContainerCenter';
 import ContainerRight from './ContainerRight';
 import useInitComponents from '../hooks/useInitComponents';
+import {Button, Layout, Divider} from 'antd';
+
+const {Header, Sider, Content, Footer} = Layout;
 
 function App(props: any) {
-  useInitComponents();
+    useInitComponents();
 
-  return (
-    <div className="k-container-wrap">
-      <ContainerLeft />
-      <ContainerCenter />
-      <ContainerRight />
-    </div>
-  );
+    return (
+        <div className="k-container-wrap">
+            <Layout>
+                <Header className="header">
+                    <div className="logo-wrap">King Low Code</div>
+                    <div className="header-btn">
+                        <Button>预览</Button>
+                        <Divider type="vertical"/>
+                        <Button>导出</Button>
+                    </div>
+                </Header>
+                <Layout>
+                    <Sider className="container-left-wrap"><ContainerLeft/></Sider>
+                    <Content className="container-content-wrap"><ContainerCenter/></Content>
+                    <Sider className="container-right-wrap"><ContainerRight/></Sider>
+                </Layout>
+                <Footer className="footer">@king</Footer>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
